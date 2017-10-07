526e1651173d45436aa9ddb9f882ef6e2c1ca281
hi363138911 <liangya363138911@126.com>
2017-10-07 11:33:37 +0800

    x

--- js/base/game.js
+++ js/base/game.js
@@ -6,18 +6,9 @@ class Game {
         this.canvas.width = window.getViewportSize.width
         this.canvas.height = window.getViewportSize.height
         this.ctx = this.canvas.getContext('2d')
-        //时间间隔
-        this.time = 0
-        //当前坐标
-        this.x = 0
-        this.y = 0
-        //要移动到的坐标
-        this.xTarget = 0
-        this.yTarget = 0
-        //移动速度
-        this.speed = 2
+       
         document.addEventListener('touchend', this.touches.bind(this), false)
-
+        // 地图与人物
         this.mapOne = new MapOne();
         this.jxk = new Jxk()
     }
@@ -25,52 +16,28 @@ class Game {
     touches(event) {
         
         //记录点击的实际位置
-        let x = this.x + event.changedTouches[0].clientX - window.getViewportSize.width / 2 
-        let y = this.y + event.changedTouches[0].clientY - window.getViewportSize.height / 2
+        let x = this.mapOne.x + event.changedTouches[0].clientX - window.getViewportSize.width / 2 
+        let y = this.mapOne.y + event.changedTouches[0].clientY - window.getViewportSize.height / 2
         
-        //判断地图边界
+        //判断地图边界，地图不移动。
         if (x < 0) x = 0
-        if (x > this.mapOne.map.width) x = this.mapOne.map.width - window.getViewportSize.width
+        if (x > this.mapOne.map.width - window.getViewportSize.width) x = this.mapOne.map.width - window.getViewportSize.width
         if (y < 0) y = 0
-        if (y > this.mapOne.map.height) x = this.mapOne.map.height - window.getViewportSize.height
+        if (y > this.mapOne.map.height - window.getViewportSize.height) y = this.mapOne.map.height - window.getViewportSize.height
         
-        this.xTarget = x
-        this.yTarget = y
+        this.mapOne.xTarget = x
+        this.mapOne.yTarget = y
 
         //jxk 要移动到的坐标
-        this.jxk.xTarget = this.xTarget
-        this.jxk.yTarget = this.yTarget
+        this.jxk.xTarget = this.mapOne.xTarget
+        this.jxk.yTarget = this.mapOne.yTarget
     }
     //每次循环调用
     draw(interval) {
-        this.time += interval
-        if (this.time > 17) {
-            this.upDataLocation()
-            this.time %= 17
-        }
-
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
         //绘制
-        this.mapOne.draw(interval, this.ctx, this.x, this.y)
-        this.jxk.draw(interval, this.ctx, this.x, this.y)
+        this.mapOne.draw(interval, this.ctx)
+        this.jxk.draw(interval, this.ctx, this.mapOne.x, this.mapOne.y)
     }
-    //更新位置
-    upDataLocation() {
-        const x = this.x - this.xTarget
-        const y = this.y - this.yTarget
-        if (x > this.speed && x != 0) {
-            this.x -= this.speed
-        } else if (x < -this.speed && x != 0) {
-            this.x += this.speed
-        } else {
-            this.x = this.xTarget
-        }
-        if (y > this.speed && y != 0) {
-            this.y -= this.speed
-        } else if (y < -this.speed && y != 0) {
-            this.y += this.speed
-        } else {
-            this.y = this.yTarget
-        }
-    }
+
 }
--- js/map_one.js
+++ js/map_one.js
@@ -1,8 +1,17 @@
 class MapOne {
     constructor() {
-        this.map
+        //当前坐标
+        this.x = 0
+        this.y = 0
+        //要移动到的坐标
+        this.xTarget = 0
+        this.yTarget = 0
+        //时间间隔
         this.time = 0
-
+        //移动速度
+        this.speed = 2
+        //地图
+        this.map
         this.init()
     }
 
@@ -11,15 +20,37 @@ class MapOne {
         this.map.src = './img/background/bg.jpg'
     }
 
-    draw(interval, ctx, x, y) {
+    draw(interval, ctx) {
+        this.time += interval
+        if (this.time > 17) {
+            this.upDataLocation()
+            this.time %= 17
+        }
+
         ctx.save()
-        const xy = this.border(x, y)
-        ctx.drawImage(this.map, -xy.x, -xy.y)
+        ctx.drawImage(this.map, -this.x, -this.y)
         ctx.restore()
     }
-    //地图边界判断
-    border(x, y) {
-        return { x, y }
+    //更新位置
+    upDataLocation() {
+        const x = this.x - this.xTarget
+        const y = this.y - this.yTarget
+        //计算移动的速速
+        if (x > this.speed && x != 0) {
+            this.x -= this.speed
+        } else if (x < -this.speed && x != 0) {
+            this.x += this.speed
+        } else {
+            this.x = this.xTarget
+        }
+        //计算移动的速度
+        if (y > this.speed && y != 0) {
+            this.y -= this.speed
+        } else if (y < -this.speed && y != 0) {
+            this.y += this.speed
+        } else {
+            this.y = this.yTarget
+        }
     }
 }
 
